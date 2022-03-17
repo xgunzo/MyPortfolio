be2bea9406c7fb179333e35e8afcd0841a32f98d
xgunzo <mchmiel07@gmail.com>
2022-03-17 01:50:30 +0100

    17.03.2022

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 window.onload = function() {
 
+    //menu
+
     let button = document.getElementById("menu-button");
     let button2 = document.getElementById("menu-button-on");
     let menu_button_display = document.getElementById("menu");
     let menu_button_on = document.getElementById("menu-button-on");
     let menu_display = document.getElementById("menu");
     let background = document.getElementById("background");
-    let websites = document.getElementById("websites");
 
-
+    let off = document.getElementById("off");
 
     button.addEventListener("click", function() {
         menu_button_display.style.display = "none";
         menu_display.style.display = "flex";
         menu_button_on.style.display = "block";
-        background.style.backgroundColor = "#121212";
-        websites.style.display = "none";
+        background.style.backgroundColor = "#212327";
+        off.style.display = "none";
     })
 
     button2.addEventListener("click", function() {
@@ -23,7 +24,34 @@ window.onload = function() {
         menu_display.style.display = "none";
         menu_button_on.style.display = "none";
         background.style.backgroundColor = "#e1dfde";
-        websites.style.display = "grid";
+
+        off.style.display = "block";
+        })
+
+    //arrow
+
+    let arrow = document.getElementById("arrowdown");
+    let image = document.getElementById("image");
+    let counter = 0;
+
+    image.addEventListener("click", function() {
+     
+        counter++;
+
+        if(counter % 2 == 0) {
+            image.src = "images/arrow-down.png";
+            arrow.href = "#top";
+            console.log("ARROW DOWN");
+            console.log(image);
+        } else if (counter % 2 !== 0) {
+            image.src = "images/arrow-up.png";
+            arrow.href = "#about";
+            console.log("ARROW UP");
+            console.log(image);
+        }
+
+        console.log(counter);
+
     })
 
 }
